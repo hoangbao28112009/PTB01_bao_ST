b852ea3f3535628b787efd1807559b334b578cad
hoangbao28112009 <nguyenphuochoangbao29@gmail.com>
2023-06-24 19:52:59 +0700

    buoi_4

---
+++ buoi_4/Buoi_4.py
@@ -0,0 +1,19 @@
+#bài 1
+n = int(float(input("Nhập n: ")))
+if (n % 2 == 0):
+    print("số chẵn")
+elif (n % 2 == 1):
+    print("số lẻ")
+
+#bài 2
+A = int(float(input("điểm trung bình môn: ")))
+if (A >= 9):
+    print("giỏi")
+elif (A == 7 or 9 or 8):
+    print("khá")
+if (A == 5 or 6 or 7):
+    print("trung bình")
+elif (A < 5):
+    print("yếu")
+    #bài 3
+
